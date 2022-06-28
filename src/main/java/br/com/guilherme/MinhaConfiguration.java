84b32ab48f9dd69d0ff22f9b7a47f4f074e458c2
guifdsk <gui.fdsk@gmail.com>
2022-06-28 00:02:07 -0300

    feat: criando ambientes e perfis de desenvolvimento

--- src/main/java/br/com/guilherme/MinhaConfiguration.java
+++ src/main/java/br/com/guilherme/MinhaConfiguration.java
@@ -1,14 +1,25 @@
 package br.com.guilherme;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.CommandLinePropertySource;
 
 @Configuration
+@Profile("dev")
 public class MinhaConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Sistemas de Vendas";
+//    @Bean(name = "applicationName")
+//    public String applicationName(){
+//        return "Sistemas de Vendas";
+//    }
+
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("rodando config de dev");
+        };
     }
 
 }
